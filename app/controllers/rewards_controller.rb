d15a0f12ec602c3822e528650a69ec22b21f7351
chau nguyen <nhbc111@yahoo.com>
2016-07-14 14:28:31 -0400

    added profile/reward form

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -2,4 +2,13 @@ class RewardsController < ApplicationController
   def new
     @rewards = Reward.new
   end
+
+  def create
+    @rewards = Reward.new
+    if @reward.save
+      render :new
+    else
+      render :new, notice: "Please add Reward for your project."
+    end
+  end
 end
