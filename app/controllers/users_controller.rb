97a9b1102c5b599323f791f12d4d6fb076ee5e33
MtGam <theresm@gmail.com>
2016-07-13 21:50:51 -0400

    added login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end 
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_url
+      redirect_to users_url, notice: "Signed up!"
     else
-      render :new
+      render :new, notice: "There was an error. Please try again."
     end
   end
 
@@ -28,4 +29,14 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+
 end
